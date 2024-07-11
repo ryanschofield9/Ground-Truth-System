7aa1a813b1579fccd9cc3484ef0e7443fab20188
ryanschofield9 <schofier@oregonstate.edu>
2024-07-11 16:36:48 -0700

    adding magnet holes to poles and updating code

--- code/video_creation.py
+++ code/video_creation.py
@@ -1,8 +1,8 @@
 import cv2
 import os
 
-image_folder = 'video_images/W1B1/full run'
-video_name = 'video_images/W1B1/full run/tree_fullrun.avi'
+image_folder = 'video_images/W2B1/full run'
+video_name = 'video_images/W2B1/full run/tree_fullrun.avi'
 
 images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
@@ -11,8 +11,9 @@ height, width, layers = frame.shape
 video = cv2.VideoWriter(video_name, 0, 1, (width,height))
 
 for num in range (0, len(images)):
-    image = 'frame' +str(num) +'.png'
+    image = 'frame' +str(num) +'scoring_white2_black1.png'
     print(image)
+    print(os.path.join(image_folder, image))
     video.write(cv2.imread(os.path.join(image_folder, image)))
     
 
